2846fe1b736b432b44718bb8387f4ab94846d21d
Zen Labs <singhvikashceg@gmail.com>
2017-02-01 22:19:28 -0800

    Cool

---
+++ sandwich/solution.cpp
@@ -0,0 +1,44 @@
+#include "sandwich.h"
+#include <message.h>
+
+#include <algorithm>
+#include <assert.h>
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <climits>
+#include <fstream>
+#include <cctype>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <map>
+#include <set>
+using namespace std;
+#define pb push_back
+#define mp make_pair
+#define bg begin()
+#define en end()
+#define Y second
+#define X first
+typedef long long ll;
+
+#define fi freopen("input.txt","r",stdin)
+#define fo freopen("output.txt","w",stdout)
+
+const double pi     =   acos(-1.0);
+const double eps    =   1e-8;
+
+#define print(a) cout<<(#a)<<" = "<<a<<"\n";
+#define fill(a,val) memset(a ,val, sizeof(a) );
+#define sz(s) ((int)(s.size()))
+
+
+int main() {
+
+	cout<<MyNodeId()<<endl;
+	return 0;
+}
